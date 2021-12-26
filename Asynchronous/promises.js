5314abb9576c2b63ca1c6aa63baa23d3ab3cd7eb
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2021-12-26 22:37:12 +0530

    promises-chamges

--- Asynchronous/promises.js
+++ Asynchronous/promises.js
@@ -1,42 +1,42 @@
-// const promise = new Promise((resolve, reject) => {
-// if (true) {
-// 	resolve("Stuff Worked");
-// } else {
-// 	reject("Error, it broke");
-// }
-// });
+const promise = new Promise((resolve, reject) => {
+	if (true) {
+		resolve("Stuff Worked");
+	} else {
+		reject("Error, it broke");
+	}
+});
 
-// promise
-// 	.then((result) => result + "!")
-//     .then((result2) => {
-//         throw Error;
-// 		console.log(result2);
-// 	})
-// 	.catch(() => {
-// 		console.log("error!");
-// 	});
+promise
+	.then((result) => result + "!")
+	.then((result2) => {
+		throw Error;
+		console.log(result2);
+	})
+	.catch(() => {
+		console.log("error!");
+	});
 
-// const promise = new Promise((resolve, reject) => {
-// 	if (true) {
-// 		resolve("Stuff Worked");
-// 	} else {
-// 		reject("Error, it broke");
-// 	}
-// });
+const promise = new Promise((resolve, reject) => {
+	if (true) {
+		resolve("Stuff Worked");
+	} else {
+		reject("Error, it broke");
+	}
+});
 
-// const promise2 = new Promise((resolve, reject) => {
-// 	setTimeout(resolve, 100, "Hii");
-// });
-// const promise3 = new Promise((resolve, reject) => {
-// 	setTimeout(resolve, 1000, "Hii2");
-// });
-// const promise4 = new Promise((resolve, reject) => {
-// 	setTimeout(resolve, 3000, "Hii3");
-// });
+const promise2 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 100, "Hii");
+});
+const promise3 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 1000, "Hii2");
+});
+const promise4 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 3000, "Hii3");
+});
 
-// Promise.all([promise, promise2, promise3, promise4]).then((value) => {
-// 	console.log(value);
-// });
+Promise.all([promise, promise2, promise3, promise4]).then((value) => {
+	console.log(value);
+});
 
 const urls = [
 	"https://jsonplaceholder.typicode.com/users",
