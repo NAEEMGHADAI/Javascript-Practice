6cc9630fc237bc8986b5115ee9a8da1d397adae5
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2021-12-27 15:20:30 +0530

    Async And Await

---
+++ Asynchronous/async&Await.js
@@ -0,0 +1,53 @@
+//ASYNC AWAIT
+
+//Syntax
+// async function playerStart() {
+// 	const first = await movePlayer(100, "Left"); //pause
+// 	const second = await movePlayer(400, "Left"); //pause
+// 	const third = await movePlayer(10, "Right"); //pause
+// 	const fourth = await movePlayer(330, "Left"); //pause
+// }
+
+fetch("https://jsonplaceholder.typicode.com/users")
+	.then((resp) => resp.json())
+	.then(console.log);
+
+async function fetchUser() {
+	const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await resp.json();
+	console.log(data);
+}
+
+Promise.all(
+	urls.map((url) => {
+		return fetch(url).then((resp) => resp.json());
+	})
+)
+	.then((results) => {
+		console.log(results[0]);
+		console.log(results[1]);
+		console.log(results[2]);
+	})
+	.catch("Oops");
+
+const urls = [
+	"https://jsonplaceholder.typicode.com/users",
+	"https://jsonplaceholder.typicode.com/albums",
+	"https://jsonplaceholder.typicode.com/posts",
+];
+
+const getData = async function () {
+	try {
+		const [user, posts, albums] = await Promise.all(
+			urls.map((url) => {
+				return fetch(url).then((resp) => resp.json());
+			})
+		);
+
+		console.log("users", user);
+		console.log("posts", posts);
+		console.log("albums", albums);
+	} catch (err) {
+		console.log("Ooops ", err);
+	}
+};
--- Asynchronous/promises.js
+++ Asynchronous/promises.js
@@ -48,8 +48,10 @@ Promise.all(
 	urls.map((url) => {
 		return fetch(url).then((resp) => resp.json());
 	})
-).then((results) => {
-	console.log(results[0]);
-	console.log(results[1]);
-	console.log(results[2]);
-});
+)
+	.then((results) => {
+		console.log(results[0]);
+		console.log(results[1]);
+		console.log(results[2]);
+	})
+	.catch("Oops");
